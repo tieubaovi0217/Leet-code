c6fc6417f9f70c70cfa0d44df2ecba04bc53bd3d
Tieu Bao <47345262+tieubaovi0217@users.noreply.github.com>
2019-11-20 11:41:45 +0700

    solution 5

--- 5-longest-palindrome.py
+++ 5-longest-palindrome.py
@@ -6,23 +6,23 @@ class Solution:
             left -= 1
             right += 1
         return ans
-
+    
     def longestPalindrome(self, s: str) -> str:
         if s == "" or len(s) < 1:
             return ""
         ans = -1
         start, end = 0, 0
-        for i in range(len(s) - 1):
-            if (len(s) - i) * 2 < ans:
+        for i in range(len(s)-1):
+            if (len(s) - i)*2 < ans:
                 continue
             l1 = self.extendPalindrome(s, i, i)
-            l2 = self.extendPalindrome(s, i, i + 1)
+            l2 = self.extendPalindrome(s, i, i+1)
             if l1 > l2 and l1 > ans:
                 ans = l1
                 start = i - (l1 - 1) // 2
                 end = i + (l1 - 1) // 2
             elif l2 > l1 and l2 > ans:
                 ans = l2
-                start = i - (l2 - 2) // 2
-                end = i + 1 + (l2 - 2) // 2
-        return s[start:end + 1]
+                start = i - (l2 - 2)//2
+                end = i + 1 + (l2 - 2)//2
+        return s[start:end+1]
